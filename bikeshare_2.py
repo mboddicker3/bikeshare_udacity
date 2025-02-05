7b90511cdbfb2c23af188c49c4cdbe82b36d7ddf
mboddicker3 <mboddic1@gmail.com>
2025-02-05 14:47:24 -0500

    Added comment to python file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -10,6 +10,7 @@ possible_months = ["january", "february", "march", "april", "may", "june", "all"
 
 possible_days = ["all", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
+# function was provided by udacity
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
